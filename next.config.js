1440e1cc2ae45f8e390dc2e57a7ed12e065bea6b
Milan <milansaxena10@gmail.com>
2023-07-19 23:02:54 -0700

    finished app

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'links.papareact.com',
+        port: '',
+      },
+      {
+        protocol: 'https',
+        hostname: 'image.tmdb.org',
+        port: '',
+      }
+    ],
+  },
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '2000px',
+      },
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+}
+
